466ddda1bea3870598811cc0cb61375b058b61ba
Eugén Cowie <eugencowie@users.noreply.github.com>
2016-03-16 23:29:10 +0000

    Tidy up generated project

--- RomanReign/Source/Program.cs
+++ RomanReign/Source/Program.cs
@@ -14,7 +14,7 @@ namespace RomanReign
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new RomanReignGame())
                 game.Run();
         }
     }
---
+++ RomanReign/Source/RomanReignGame.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RomanReign
+{
+    /// <summary>
+    /// This is the main class for the game.
+    /// </summary>
+    public class RomanReignGame : Game
+    {
+        GraphicsDeviceManager m_graphics;
+        SpriteBatch m_spriteBatch;
+
+        public RomanReignGame()
+        {
+            m_graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = 1280,
+                PreferredBackBufferHeight = 720
+            };
+
+            IsMouseVisible = true;
+
+            Content.RootDirectory = "Content";
+        }
+        
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            m_spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+        
+        protected override void UnloadContent()
+        {
+        }
+        
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
