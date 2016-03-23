2528a2533de05a408fd80649e90835039094bcc6
Eugén Cowie <eugencowie@users.noreply.github.com>
2016-03-23 09:59:35 +0000

    Add sprite class

---
+++ RomanReign/Source/Utilities/Sprite.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RomanReign
+{
+    class Sprite
+    {
+        public Texture2D Texture = null;
+        public Vector2 Origin = Vector2.Zero;
+        public Color Color = Color.White;
+
+        public Vector2 Position = Vector2.Zero;
+        public Vector2 Scale = Vector2.One;
+        public float UniformScale = 1f;
+
+        public SpriteEffects Effects = SpriteEffects.None;
+
+        public bool Visible = true;
+
+        private Vector2 TopLeft
+        {
+            get { return Position - (Origin * (Scale * UniformScale)); }
+        }
+
+        private Vector2 Size
+        {
+            get { return Texture.Bounds.Size.ToVector2() * (Scale * UniformScale); }
+        }
+
+        public Rectangle Bounds
+        {
+            get { return new Rectangle((int)TopLeft.X, (int)TopLeft.Y, (int)Size.X, (int)Size.Y); }
+        }
+
+        public Sprite(Texture2D texture)
+        {
+            Texture = texture;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (Visible && Texture != null)
+            {
+                spriteBatch.Draw(
+                    Texture,
+                    Position,
+                    null,
+                    Color,
+                    0f,
+                    Origin,
+                    Scale * UniformScale,
+                    Effects,
+                    0f);
+            }
+        }
+    }
+}
