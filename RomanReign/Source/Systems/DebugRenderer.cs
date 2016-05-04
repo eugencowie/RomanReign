011659658b02fcc9478aa9ab75f6ca28ac7f8686
Eugén Cowie <eugencowie@users.noreply.github.com>
2016-05-04 10:03:29 +0100

    Refactor debug renderer

--- RomanReign/Source/Systems/DebugRenderer.cs
+++ RomanReign/Source/Systems/DebugRenderer.cs
@@ -3,18 +3,22 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace RomanReign
 {
+    /// <summary>
+    /// Provides a way to draw debug information.
+    /// </summary>
     class DebugRenderer
     {
         public bool Enabled;
 
         SpriteBatch m_spriteBatch;
-        Texture2D   m_debugPixel;
+        Texture2D m_debugPixel;
 
         public DebugRenderer(GraphicsDevice device, SpriteBatch spriteBatch)
         {
 #if DEBUG
             m_spriteBatch = spriteBatch;
 
+            // Create a 1x1 pixel semi-transparent white texture.
             m_debugPixel = new Texture2D(device, 1, 1, false, SurfaceFormat.Color);
             Color[] color = new Color[1];
             color[0] = new Color(Color.White, 127);
@@ -22,14 +26,7 @@ namespace RomanReign
 #endif
         }
 
-        public void Draw(Vector2 position, Vector2 size, Color? color=null)
-        {
-#if DEBUG
-            Draw(new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y));
-#endif
-        }
-
-        public void Draw(Rectangle rectangle, Color? color=null)
+        public void Draw(Rectangle rectangle, Color? color = null)
         {
 #if DEBUG
             if (Enabled && m_spriteBatch != null && m_debugPixel != null)
@@ -37,6 +34,16 @@ namespace RomanReign
                 m_spriteBatch.Draw(m_debugPixel, rectangle, color ?? Color.Red);
             }
 #endif
+        }
+
+        public void Draw(Point position, Point size, Color? color = null)
+        {
+            Draw(new Rectangle(position, size), color);
+        }
+
+        public void Draw(Vector2 position, Vector2 size, Color? color = null)
+        {
+            Draw(new Rectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y), color);
         }
     }
 }
