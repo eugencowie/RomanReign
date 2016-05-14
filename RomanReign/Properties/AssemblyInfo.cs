9bfe22b1d2b07f975bcc00316e36ab4fe9081fcc
Eugén Cowie <eugencowie@users.noreply.github.com>
2016-05-14 21:44:28 +0100

    Update assembly info

--- RomanReign/Properties/AssemblyInfo.cs
+++ RomanReign/Properties/AssemblyInfo.cs
@@ -4,21 +4,24 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RomanReign")]
-[assembly: AssemblyProduct("RomanReign")]
+
+[assembly: AssemblyTitle("Roman Reign")]
+[assembly: AssemblyProduct("Roman Reign")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("9a01f978-702d-43ae-b333-2a701c440fa4")]
 
 // Version information for an assembly consists of the following four values:
@@ -31,5 +34,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.4.0.456")]
-[assembly: AssemblyFileVersion("3.4.0.456")]
+
+[assembly: AssemblyVersion("0.11")]
+[assembly: AssemblyFileVersion("0.11")]
