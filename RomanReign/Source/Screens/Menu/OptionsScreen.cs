d60cb0eaa3691eefffe335e39d6756a0f6dbfd02
Eugén Cowie <eugencowie@users.noreply.github.com>
2016-05-26 21:57:54 +0100

    Update options screen with new buttons

--- RomanReign/Source/Screens/Menu/OptionsScreen.cs
+++ RomanReign/Source/Screens/Menu/OptionsScreen.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -19,11 +20,18 @@ namespace RomanReign
 
         // We need a sprite for the back button.
 
+        Sprite m_musicButton;
+        Sprite m_sfxButton;
         Sprite m_backButton;
 
         Texture2D m_buttonBackground;
 
+        enum SelectedButton { None, Music, Sfx, Back, Final }
+        SelectedButton m_selectedButton;
+
         SpriteFont m_font;
+
+        SoundEffect m_selectedSound;
 
         /// <summary>
         /// This constructor is run when the options menu screen object is created.
@@ -40,18 +48,36 @@ namespace RomanReign
         {
             // See Screens/Menu/MenuSreen.cs for an explanation of how sprite origins work.
 
-            m_backButton = new Sprite(content.Load<Texture2D>("Textures/Menu/btn_back"));
-            m_backButton.SetRelativeScale(0.75f, 0.75f);
-            m_backButton.SetRelativeOrigin(0.5f, 0.5f);
+            m_musicButton = new Sprite(content.Load<Texture2D>("Textures/Menu/btn_options_music"));
+            m_musicButton.SetRelativeScale(0.75f, 0.75f);
+            m_musicButton.SetRelativeOrigin(0.5f, 0.5f);
 
-            float posX = m_game.Viewport.Right - (m_backButton.Bounds.Size.X / 2f) - 50;
-            float posY = m_game.Viewport.Height - (m_backButton.Bounds.Size.Y / 2f) - 50;
+            float posX = m_game.Viewport.Right - (m_musicButton.Bounds.Size.X / 2f) - 50;
+            float posY = m_game.Viewport.Height - (m_musicButton.Bounds.Size.Y / 2f) - 50 - (75 * 2);
 
-            m_backButton.Position = new Vector2(posX, posY);
+            m_musicButton.Position = new Vector2(posX, posY);
+
+            m_sfxButton = new Sprite(content.Load<Texture2D>("Textures/Menu/btn_options_sfx")) {
+                Position = new Vector2(posX, posY += 75)
+            };
+
+            m_backButton = new Sprite(content.Load<Texture2D>("Textures/Menu/btn_back")) {
+                Position = new Vector2(posX, posY += 75)
+            };
+
+            foreach (Sprite button in new[] { m_sfxButton, m_backButton })
+            {
+                button.SetRelativeScale(0.75f, 0.75f);
+                button.SetRelativeOrigin(0.5f, 0.5f);
+            }
 
             m_buttonBackground = content.Load<Texture2D>("Textures/Menu/btn_background");
 
-            m_font = content.Load<SpriteFont>("Fonts/game");
+            m_font = content.Load<SpriteFont>("Fonts/menu");
+
+            m_selectedButton = SelectedButton.None;
+
+            m_selectedSound = content.Load<SoundEffect>("Audio/sfx_menu_select");
         }
 
         /// <summary>
@@ -67,29 +93,63 @@ namespace RomanReign
         /// </summary>
         public void Update(GameTime gameTime)
         {
-            if (m_game.Input.MostRecentInputType == InputType.Gamepad)
-                m_backButton.SetOpacity(0.5f);
-
             if (m_game.Input.MostRecentInputType == InputType.KBM)
             {
-                bool mouseOver = m_backButton.Bounds.Contains(m_game.Input.Mouse.Position);
-                m_backButton.SetOpacity(mouseOver ? 0.5f : 1f);
-            }
+                m_selectedButton = SelectedButton.None;
 
-            if (m_game.Input.IsJustReleased(Keys.Escape) ||
-                m_game.Input.IsJustReleased(Buttons.B) ||
-                m_game.Input.IsJustReleased(Buttons.A))
-            {
-                m_game.Screens.Pop();
-            }
+                if (m_musicButton.Bounds.Contains(m_game.Input.Mouse.Position))
+                    m_selectedButton = SelectedButton.Music;
 
-            if (m_game.Input.IsJustReleased(MouseButtons.Left))
-            {
+                if (m_sfxButton.Bounds.Contains(m_game.Input.Mouse.Position))
+                    m_selectedButton = SelectedButton.Sfx;
+
                 if (m_backButton.Bounds.Contains(m_game.Input.Mouse.Position))
+                    m_selectedButton = SelectedButton.Back;
+            }
+
+            if (m_game.Input.MostRecentInputType == InputType.Gamepad)
+            {
+                if (m_game.Input.IsJustPressed(Buttons.DPadUp) || m_game.Input.IsJustPressed(Buttons.LeftThumbstickUp))
+                {
+                    m_selectedButton--;
+                }
+
+                if (m_game.Input.IsJustPressed(Buttons.DPadDown) || m_game.Input.IsJustPressed(Buttons.LeftThumbstickDown))
+                {
+                    m_selectedButton++;
+                }
+
+                if (m_selectedButton == SelectedButton.None)
+                    m_selectedButton = SelectedButton.None + 1;
+
+                if (m_selectedButton == SelectedButton.Final)
+                    m_selectedButton = SelectedButton.Final - 1;
+            }
+
+            m_musicButton.SetOpacity(m_selectedButton == SelectedButton.Music ? 0.5f : 1f);
+            m_sfxButton.SetOpacity(m_selectedButton == SelectedButton.Sfx ? 0.5f : 1f);
+            m_backButton.SetOpacity(m_selectedButton == SelectedButton.Back ? 0.5f : 1f);
+
+            if (m_game.Input.IsJustReleased(MouseButtons.Left) || m_game.Input.IsJustReleased(Buttons.A))
+            {
+                if (m_selectedButton == SelectedButton.Music)
+                {
+                    m_selectedSound.Play(0.25f * Config.Data.Volume.SfxNormal, 0f, 0f);
+                }
+
+                if (m_selectedButton == SelectedButton.Sfx)
+                {
+                    m_selectedSound.Play(0.25f * Config.Data.Volume.SfxNormal, 0f, 0f);
+                }
+
+                if (m_selectedButton == SelectedButton.Back)
                 {
                     m_game.Screens.Pop();
+                    m_selectedSound.Play(0.25f * Config.Data.Volume.SfxNormal, 0f, 0f);
                 }
             }
+
+
 
             if (m_game.Input.IsJustReleased(Keys.Q))
             {
@@ -117,11 +177,14 @@ namespace RomanReign
         {
             spriteBatch.Begin();
 
-            spriteBatch.Draw(m_buttonBackground, m_backButton.Bounds, Color.White);
-            m_backButton.Draw(spriteBatch);
+            foreach (Sprite button in new[] { m_musicButton, m_sfxButton, m_backButton })
+            {
+                spriteBatch.Draw(m_buttonBackground, button.Bounds, Color.White);
+                button.Draw(spriteBatch);
+            }
 
-            spriteBatch.DrawString(m_font, "Music volume: " + Config.Data.Volume.Music + "%", new Vector2(100, 100), Color.White);
-            spriteBatch.DrawString(m_font, "Sound effect volume: " + Config.Data.Volume.Sfx + "%", new Vector2(100, 200), Color.White);
+            spriteBatch.DrawString(m_font, "Music volume: " + Config.Data.Volume.Music + "%", new Vector2(100, 100), Color.Black);
+            spriteBatch.DrawString(m_font, "Sound effect volume: " + Config.Data.Volume.Sfx + "%", new Vector2(100, 200), Color.Black);
 
             spriteBatch.End();
         }
